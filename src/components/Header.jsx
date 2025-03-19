d68ada24afc9601bec429a59594a6a6bb5186d15
sahil <sahils.verma.1000@gmail.com>
2025-03-19 14:06:17 +0530

    nav-bar-complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,52 @@
 import React from "react";
-import { HiHome, HiMagnifyingGlass, HiPlayCircle, HiPlus, HiTv } from "react-icons/hi2";
+import {
+  HiHome,
+  HiMagnifyingGlass,
+  HiPlayCircle,
+  HiPlus,
+  HiTv,
+} from "react-icons/hi2";
 import HeaderItem from "./HeaderItem";
 function Header() {
   const menu = [
     {
       name: "HOME",
-      icon:HiHome
+      icon: HiHome,
     },
     {
-      name:"SEARCH",
-      icon:HiMagnifyingGlass
+      name: "SEARCH",
+      icon: HiMagnifyingGlass,
     },
     {
-      name:"WATCH LIST",
-      icon:HiPlus
+      name: "WATCH LIST",
+      icon: HiPlus,
     },
     {
-      name:"MOVIES",
-      icon:HiPlayCircle
-
+      name: "MOVIES",
+      icon: HiPlayCircle,
     },
     {
-      name:"SERIES",
-      icon:HiTv
-    }
-  ]
+      name: "SERIES",
+      icon: HiTv,
+    },
+  ];
   return (
-    <div>
-      <img
-        src=" https://logowik.com/content/uploads/images/disney-hotstar3268.jpg"
-        alt=""
-        className="w-[80px] md:width[115px] object-cover"
-        srcSet=""
-      />.
-      {menu.map((item)=>(
-        <HeaderItem key={item.name} name={item.name} Icon={item.icon}/>
-      ))}
+    <div className="flex items-center gap-8 justify-between p-5">
+      <div className="flex items-center gap-8 items-center" >
+        <img
+          src=" https://logowik.com/content/uploads/images/disney-hotstar3268.jpg"
+          alt=""
+          className="w-[80px] md:width[115px] object-cover"
+          srcSet=""
+        />
+        .
+        {menu.map((item) => (
+          <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
+        ))}
+      </div>
+      <img src="https://img.freepik.com/premium-vector/avatar-profile-icon-flat-style-female-user-profile-vector-illustration-isolated-background-women-profile-sign-business-concept_157943-38866.jpg?semt=ais_hybrid" className="w-[40px] rounded-full" alt="" />
     </div>
   );
 }
 
 export default Header;
-
--- src/components/HeaderItem.jsx
+++ src/components/HeaderItem.jsx
@@ -3,7 +3,8 @@ import React from "react";
 export default function HeaderItem({ name, Icon }) {
   console.log(name);
   return (
-    <div className="">
+    <div className=" flex items-center gap-3 text-[15px] font-semibold cursor-pointer hover:underline
+    underline-offset-8">
         {/* {"hello"} */}
       <Icon />
       <h2>{name}</h2>
